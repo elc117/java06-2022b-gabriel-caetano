fecb70a15f50829dfe54f58cdf7f2b69f84a3c63
Gabriel Caetano <gabrielvscaetano@gmail.com>
2022-10-11 11:46:54 -0300

    fix: use super message on group assignment aletrnative message

--- GroupAssignment.java
+++ GroupAssignment.java
@@ -4,7 +4,7 @@ class GroupAssignment extends Assignment {
   private String teamMates;
 
   public String message() {
-    return "Group Assignment " + this.description + " is " + this.status() + " call " + this.teamMates;    
+    return super.message() + " call " + this.teamMates;    
   }
 
   public GroupAssignment(LocalDate dueDate, String description, String teamMates) {
