34d79528a421c136ec2857588081e545152838a6
Gabriel Caetano <gabrielvscaetano@gmail.com>
2022-10-11 10:56:50 -0300

    feat: add template files

---
+++ GroupAssignment.java
@@ -0,0 +1,17 @@
+import java.time.LocalDate;
+
+class GroupAssignment extends Assignment {
+    private String teamMates;
+  
+    public String message() {
+      return "COMPLETE-ME";
+    
+    }
+  
+    public GroupAssignment(LocalDate dueDate, String description, String teamMates) {
+      super(dueDate, description);
+      this.teamMates = teamMates;
+    }
+  
+    
+  }
