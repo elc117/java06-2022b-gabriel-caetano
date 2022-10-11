91a74434a9717bc8b487a138c40b7674ae13dddd
Gabriel Caetano <gabrielvscaetano@gmail.com>
2022-10-11 11:28:21 -0300

    feat: group assignment alternative message

--- GroupAssignment.java
+++ GroupAssignment.java
@@ -1,17 +1,14 @@
 import java.time.LocalDate;
 
 class GroupAssignment extends Assignment {
-    private String teamMates;
-  
-    public String message() {
-      return "COMPLETE-ME";
-    
-    }
-  
-    public GroupAssignment(LocalDate dueDate, String description, String teamMates) {
-      super(dueDate, description);
-      this.teamMates = teamMates;
-    }
-  
-    
+  private String teamMates;
+
+  public String message() {
+    return "Group Assignment " + this.description + " is " + this.status() + " call " + teamMates;    
   }
+
+  public GroupAssignment(LocalDate dueDate, String description, String teamMates) {
+    super(dueDate, description);
+    this.teamMates = teamMates;
+  } 
+}
